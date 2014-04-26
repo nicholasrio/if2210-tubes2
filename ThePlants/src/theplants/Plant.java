a45e16e461bc17d6d6ac953ad9660127daa9094c
azaky <a_zaky003@yahoo.com>
2014-04-26 23:10:10 +0700

    First Commit

---
+++ ThePlants/src/theplants/Plant.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package theplants;
+
+/**
+ *
+ * @author user
+ */
+public class Plant {
+    
+    public double hargabibit(){
+        //ini stub
+        return 2000;
+    }
+    
+}
---
+++ ThePlants/src/theplants/Player.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package theplants;
+import java.util.*;
+import java.io.*;
+/**
+ *
+ * @author user
+ */
+public class Player {
+    private double Money;
+    private String ID;
+    private ArrayList<Plant> ULPlant;
+    private ArrayList<Vehicle> ULVehicle;
+    private ArrayList<Fertilizer> ULFertilizer;
+    
+    
+    public Player(){
+        Money=100;
+        ID="Player";
+        ULPlant=new ArrayList<Plant>();
+        ULVehicle=new ArrayList<Vehicle>();
+        ULFertilizer=new ArrayList<Fertilizer>();
+    }
+    public void ChangeID(String nama){
+        ID=nama;
+    }
+    public void beli(Plant P){
+        Money=Money-P.hargabibit();
+    }
+    public void addOb(Plant OB){
+       ULPlant.add(OB);
+    }
+    public void addOb(Vehicle OB){
+       ULVehicle.add(OB);
+    }
+    public void addOb(Fertilizer OB){
+       ULFertilizer.add(OB);
+    }
+}
