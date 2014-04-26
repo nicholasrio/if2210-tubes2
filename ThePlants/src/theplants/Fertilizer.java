087443884184cf18ccddc6ad01a63715360336b6
William Stefan <williamstefanh@yahoo.com>
2014-04-27 00:12:08 +0700

    Realisasi Fertilizer.java dan Vehicle.java oleh William Stefan

--- ThePlants/src/theplants/Fertilizer.java
+++ ThePlants/src/theplants/Fertilizer.java
@@ -1,15 +1,56 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package theplants;
 
 /**
  *
- * @author user
+ * @author William Stefan
  */
-public class Fertilizer {
+import java.util.*;
+
+public class Fertilizer
+{
+    private int level, buyprice;
     
+    public Fertilizer()
+    {
+        this.level = 0;
+        this.buyprice = 0;
+    }
+    
+    public Fertilizer(int new_level, int new_buyprice)
+    {
+        this.level = new_level;
+        this.buyprice = new_buyprice;
+    }
+    
+    public void setlevel(int new_level)
+    {
+        this.level = new_level;
+    }
+    
+    public void setbuyprice(int new_buyprice)
+    {
+        this.buyprice = new_buyprice;
+    }
+    
+    public int getlevel()
+    {
+        return (this.level);
+    }
+    
+    public int getbuyprice()
+    {
+        return (this.buyprice);
+    }
+    
+    public void print()
+    {
+        System.out.println("level: " + level);
+        System.out.println("buyprice: " + buyprice);
+    }
 }
--- ThePlants/src/theplants/Vehicle.java
+++ ThePlants/src/theplants/Vehicle.java
@@ -1,15 +1,56 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 package theplants;
 
 /**
  *
- * @author user
+ * @author William Stefan
  */
-public class Vehicle {
+import java.util.*;
+
+public class Vehicle
+{
+    private int speed, capacity;
     
+    public Vehicle()
+    {
+        this.speed = 0;
+        this.capacity = 0;
+    }
+    
+    public Vehicle(int new_speed, int new_capacity)
+    {
+        this.speed = new_speed;
+        this.capacity = new_capacity;
+    }
+    
+    public void setspeed(int new_speed)
+    {
+        this.speed = new_speed;
+    }
+    
+    public void setcapacity(int new_capacity)
+    {
+        this.capacity = new_capacity;
+    }
+    
+    public int getspeed()
+    {
+        return (this.speed);
+    }
+    
+    public int getcapacity()
+    {
+        return (this.capacity);
+    }
+    
+    public void print()
+    {
+        System.out.println("speed: " + speed);
+        System.out.println("capacity: " + capacity);
+    }
 }
