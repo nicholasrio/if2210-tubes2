6f1f68ff301f5c66148e3d503e8a438105cae9c2
dalva <dalva@AestariaLinux>
2014-05-11 11:38:13 +0700

    XML updates, GUI updates

--- src/calogerusdraconis/View.java
+++ src/calogerusdraconis/View.java
@@ -17,30 +17,18 @@
 
 package calogerusdraconis;
 
-
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-// #[regen=yes,id=DCE.95E31C0A-5A00-66DF-D726-38397C93359E]
-// </editor-fold> 
+/**
+ *
+ * @author Dariel Valdano
+ */
 public interface View {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.481C5C9C-569A-4681-602E-F139067CDDC3]
-    // </editor-fold> 
     public void UpdateScreen (UserDragon drg);
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.F596E244-8EE3-240E-ADD8-7908C8F36291]
-    // </editor-fold> 
     public void UpdateScreen (Store str);
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.41CD8BC4-F1D5-8CE9-F183-ED13062BE962]
-    // </editor-fold> 
     public void UpdateScreen (UserDragon drg, Dragon enemy);
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.CAE25CD5-671E-A7E1-88F1-BF1E64D97987]
-    // </editor-fold> 
     public void UpdateScreen (UserDragon drg, Event evnt);
 
 }
