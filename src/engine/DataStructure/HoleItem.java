898a7a2598c5c0333c90b6f236dea67a43b37731
Winson <13512071@std.stei.itb.ac.id>
2014-05-12 23:41:59 +0700

    Tambah Item Map

---
+++ src/engine/DataStructure/HoleItem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package engine.DataStructure;
+
+/**
+ *
+ * @author Winson
+ */
+public class HoleItem extends GameItem{
+    public HoleItem(){
+        super("hole");
+    }
+}
---
+++ src/engine/DataStructure/WallItem.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package engine.DataStructure;
+
+/**
+ *
+ * @author Winson
+ */
+public class WallItem extends GameItem{
+    public WallItem(){
+        super("wall");
+    }
+}
