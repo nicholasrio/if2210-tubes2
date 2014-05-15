95f6aa587a1170083131a8dce4ba74cd93075d62
Tegar <tegarnization@gmail.com>
2014-05-15 16:49:54 +0700

    Changes 15/05/2014

--- VideoPlayer/YoutubeMediaPlayer.java
+++ VideoPlayer/YoutubeMediaPlayer.java
@@ -1,48 +1,29 @@
 package VideoPlayer;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
-import javax.swing.WindowConstants;
-
-import chrriis.dj.nativeswing.swtimpl.NativeInterface;
 import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
 
 public class YoutubeMediaPlayer {
 
-	public static void main(String[] args) 
-{
-    NativeInterface.open();
-    SwingUtilities.invokeLater(new Runnable() 
-    {	
-        public void run() 
-        {
-            JFrame frame = new JFrame("YouTube Viewer");
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-            frame.getContentPane().add(getBrowserPanel(), BorderLayout.CENTER);
-            frame.setSize(800, 600);
-            frame.setLocationByPlatform(true);
-            frame.setVisible(true);
-        }
-    });
-    NativeInterface.runEventPump();
-    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-        @Override
-        public void run() {
-            NativeInterface.close();
-        }
-    }));
-}
-
-public static JPanel getBrowserPanel() 
-{
-    JPanel webBrowserPanel = new JPanel(new BorderLayout());
-    JWebBrowser webBrowser = new JWebBrowser();
-    webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
-    webBrowser.setBarsVisible(false);
-    webBrowser.navigate("http://www.youtube.com/watch?v=JnRYo75KSyQ");
-    return webBrowserPanel;
-}
+	public static JPanel getBrowserPanel(String VideoURL) 
+	{
+	    JPanel webBrowserPanel = new JPanel(new BorderLayout());
+	    JWebBrowser webBrowser = new JWebBrowser();
+	    webBrowser.setBarsVisible(false);
+	    webBrowser.setMenuBarVisible(false);
+	    webBrowser.setAutoscrolls(false);
+	    webBrowserPanel.add(webBrowser, BorderLayout.CENTER);
+	    webBrowser.setBarsVisible(false);
+	    Dimension dimension = new Dimension();
+	    dimension.height = 675;
+	    dimension.width = 550;
+	    webBrowser.navigate(VideoURL);
+	    webBrowser.setButtonBarVisible(false);
+	    webBrowser.setAutoscrolls(false);
+	    webBrowserPanel.setAutoscrolls(false);
+	    return webBrowserPanel;
+	}
 }
