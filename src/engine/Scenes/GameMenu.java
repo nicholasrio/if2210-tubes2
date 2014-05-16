feed2dc70ab48cb3d69932d475b3d128ebb0c9cb
Winson <13512071@std.stei.itb.ac.id>
2014-05-16 22:01:43 +0700

    Tambah pesan waktu main selesai

--- src/engine/Scenes/GameMenu.java
+++ src/engine/Scenes/GameMenu.java
@@ -23,6 +23,8 @@ public class GameMenu extends Scene
     private final int MAXCOLCANVAS = 166;
     private int MAPROWOFFSET;
     private int MAPCOLOFFSET;
+    private boolean isFinish;
+    private boolean isLevelUnlocked;
     
     public GameMenu()
     {
@@ -42,39 +44,75 @@ public class GameMenu extends Scene
         player.initPlayerPosition(gameMap);
         player.resetTempScore();
         
+        isFinish = false;
+        isLevelUnlocked = false;
+        
         canvas = new CLICanvas(MAXROWCANVAS,MAXCOLCANVAS);
     }
     
     @Override
     public void Update()
     {
-        System.out.println("Enter 1(up) 2(down) 3(left) 4(right) to move, 5 to return, 6 to exit : ");
         Scanner Sc = new Scanner(System.in);
-        int choise = Sc.nextInt();
-        switch(choise){
-            case 1 : player.move(gameMap,1);
-                     break;
-            case 2 : player.move(gameMap,2);
-                     break;
-            case 3 : player.move(gameMap,3);
-                     break;
-            case 4 : player.move(gameMap,4);
-                     break;
-            case 5 : SceneManager.SwitchScene("LevelMenu");
-                     break;
-            case 6 : Game.gameRunning = false;
-                     break;
+        if(isFinish){
+            //Press any key to continue
+            Sc.nextLine();
+            SceneManager.SwitchScene("LevelMenu");
+        }else{
+            //Pilihan perintah pada GameMenu
+            int choise = Sc.nextInt();
+            int output = 0;
+            switch(choise){
+                case 1 : output = player.move(gameMap,1);
+                         break;
+                case 2 : output = player.move(gameMap,2);
+                         break;
+                case 3 : output = player.move(gameMap,3);
+                         break;
+                case 4 : output = player.move(gameMap,4);
+                         break;
+                case 5 : SceneManager.SwitchScene("LevelMenu");
+                         break;
+                case 6 : Game.gameRunning = false;
+                         break;
+            }
+            if(output == 1){
+                isFinish = true;
+                if(GameData.nowLevelPlayed == player.getLevelUnlocked()){
+                    player.setLevelUnlocked(player.getLevelUnlocked()+1);
+                    isLevelUnlocked = true;
+                }
+            }
         }
     }
     
     @Override
     public void Draw()
     {
-        canvas.clear();
-        Location L = player.getLocation();
-        System.out.println("Floor: "+L.getLevel());
-        gameMap.Draw(canvas,L.getLevel(),MAPROWOFFSET, MAPCOLOFFSET);
-        player.Draw(canvas, MAPROWOFFSET+L.getRow()*3, MAPCOLOFFSET+L.getCol()*3);
-        canvas.repaint();
+        if(isFinish){
+            canvas.clear();
+            String S;
+            S = "Congratulation";
+            canvas.writeHorisontal(20, (MAXROWCANVAS - S.length())/2 , S);
+            S = "Score = "+player.getTempScore();
+            canvas.writeHorisontal(21, (MAXROWCANVAS - S.length())/2 , S);
+            if(isLevelUnlocked){
+                S = "Level Unlocked!!!";
+                canvas.writeHorisontal(22, (MAXROWCANVAS - S.length())/2 , S);
+                S = "Level "+player.getLevelUnlocked();
+                canvas.writeHorisontal(23, (MAXROWCANVAS - S.length())/2 , S);
+                S = "Press Enter to Continue";
+                canvas.writeHorisontal(24, (MAXROWCANVAS - S.length())/2 , S);
+            } 
+            canvas.repaint();
+        } else{
+            canvas.clear();
+            Location L = player.getLocation();
+            System.out.println("Floor: "+L.getLevel()+"; Score: "+player.getTempScore());
+            gameMap.Draw(canvas,L.getLevel(),MAPROWOFFSET, MAPCOLOFFSET);
+            player.Draw(canvas, MAPROWOFFSET+L.getRow()*3, MAPCOLOFFSET+L.getCol()*3);
+            canvas.repaint();
+            System.out.println("Enter 1(up) 2(down) 3(left) 4(right) to move, 5 to return, 6 to exit : ");
+        }
     }
 }
