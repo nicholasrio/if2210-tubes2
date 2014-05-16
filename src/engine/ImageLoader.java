41de17e61f290768d4dbc6b000dbb65643d55bb6
Tony <13512018@std.stei.itb.ac.id>
2014-05-17 00:05:03 +0700

    Merge origin/master

--- src/engine/ImageLoader.java
+++ src/engine/ImageLoader.java
@@ -28,7 +28,8 @@ public class ImageLoader extends JPanel
     
     public static void loadAllImages()
     {
-        allImages.add(loadImage("Image/title.png"));
+        // Image buat MainMenu
+        allImages.add(loadImage("Image/MainMenu/title.png"));
         allImages.add(loadImage("Image/MainMenu/mainmenu_bg.png"));
         allImages.add(loadImage("Image/MainMenu/newgame.png"));
         allImages.add(loadImage("Image/MainMenu/achievement.png"));
@@ -38,18 +39,11 @@ public class ImageLoader extends JPanel
         allImages.add(loadImage("Image/MainMenu/back.png"));
         
         //Image buat level menu
-        //Image ke-8
         allImages.add(loadImage("Image/LevelMenu/background.png"));
-        
-        //Image ke-9
-        allImages.add(loadImage("Image/LevelMenu/cube_mazer(ice).png"));        
-        
-        //Image ke-10
+        allImages.add(loadImage("Image/LevelMenu/cube_mazer(ice).png")); 
         allImages.add(loadImage("Image/LevelMenu/cube_mazer(fire).png"));
-    
         allImages.add(loadImage("Image/AchievementBack.png"));
-
-}
+    }
     
     public static Image getImage(String imgName)
     {
