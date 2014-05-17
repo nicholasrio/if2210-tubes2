c52e9ea518835765f6434ff0ea32aee9ac50a0c5
melvinfonda <fondamelvin@gmail.com>
2014-05-17 16:10:39 +0700

    revisi monster 2

--- towerDefense/src/monster GUI/Monster.java
+++ towerDefense/src/monster GUI/Monster.java
@@ -1,19 +1,7 @@
-<<<<<<< HEAD
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
-=======
-/**
- *
- * @author 
- * Darwin Prasetio (13512015)
- * Chrestella Stephanie (13512005)
- * Jan Wira Gotama Putra (13512015)
- * Eric (13512021)
- * Willy(13512070)
- * Melvin Fonda (13512085)
->>>>>>> ec51448d4cab0a8b4fa39178a8f0025ef448dace
  */
 
 package monster;
@@ -44,18 +32,13 @@ public class Monster {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 400);
         frame.setLocationRelativeTo(null);
-<<<<<<< HEAD
         frame.add(new MonsterCanvas());
-=======
-        frame.add(new TrainCanvas());
->>>>>>> ec51448d4cab0a8b4fa39178a8f0025ef448dace
         
         frame.setVisible(true);
     }
 
 }
 
-<<<<<<< HEAD
 class MonsterCanvas extends JComponent {
     public static int TempN=0;
     private int lastX = 0;
@@ -64,6 +47,8 @@ class MonsterCanvas extends JComponent {
     private static final int rows = 5;
     private static final int cols = 5;
 
+    
+    
     
     public MonsterCanvas() {
                 BufferedImage bigImg = null;
@@ -90,15 +75,6 @@ class MonsterCanvas extends JComponent {
         }
  
         
-=======
-class TrainCanvas extends JComponent {
-    public static int TempN=0;
-    private int lastX = 0;
-    private int lastY = 0;
-   private Timer timer;
-    
-    public TrainCanvas() {
->>>>>>> ec51448d4cab0a8b4fa39178a8f0025ef448dace
         Thread animationThread = new Thread(new Runnable() {
             @Override
             public void run() {
@@ -136,18 +112,10 @@ class TrainCanvas extends JComponent {
         int h = getHeight();
         int trainW=100;
         int x = lastX;
-<<<<<<< HEAD
-        int x1 = lastX;
         int y = lastY;
-        int y1 = lastY;
         int Speed = 1;
         
         int temp = 1;
-=======
-        int y = lastY;
-        int Speed = 1;
-        int temp = 2;
->>>>>>> ec51448d4cab0a8b4fa39178a8f0025ef448dace
         if(temp == 1)
         {
            x  += Speed;
@@ -157,25 +125,16 @@ class TrainCanvas extends JComponent {
         {
             y  += Speed;
         }
-<<<<<<< HEAD
         else if (temp == 3)
         {
             x  -= Speed;
         }
         else if (temp == 4)
-=======
-        else if (temp == 4)
-        {
-            x  -= Speed;
-        }
-        else if (temp == 8)
->>>>>>> ec51448d4cab0a8b4fa39178a8f0025ef448dace
         {
             y  -= Speed;
         }
         
 
-<<<<<<< HEAD
         if (x > w + 100) {
             x = -trainW;
         }
@@ -186,41 +145,6 @@ class TrainCanvas extends JComponent {
         gg.drawImage(sprites[(2*rows)+TempN%5], x, y+140, this);
         gg.drawImage(sprites[(3*rows)+TempN%5], x, y+210, this);
         gg.drawImage(sprites[(4*rows)+TempN%5], x, y+280, this);
-=======
-        if (x > w + trainW) {
-            x = 0;
-        }
-        
-        
-        BufferedImage bigImg = null;
-        try {
-            bigImg = ImageIO.read(new File("img/monster.png"));
-        } catch (IOException ex) {
-            Logger.getLogger(TrainCanvas.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        final int width = 50;
-        final int height = 60;
-        final int rows = 5;
-        final int cols = 5;
-        BufferedImage[] sprites = new BufferedImage[rows * cols];
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                sprites[(i * cols) + j] = bigImg.getSubimage(
-                    j * width,
-                    i * height,
-                    width,
-                    height
-                );
-            }
-        }
-        gg.drawImage(sprites[TempN%5], x, y, this);
-        gg.drawImage(sprites[TempN%5], x, y+70, this);
-//        gg.setColor(Color.BLACK);
-//        gg.fillRect(x, h/2 + trainH, trainW, trainH);
->>>>>>> ec51448d4cab0a8b4fa39178a8f0025ef448dace
 
         lastX = x;
         lastY = y;
