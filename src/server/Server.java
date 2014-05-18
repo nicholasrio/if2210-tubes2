29d6b395a11afa5688149c332e38472d930aae75
adwisatya <a.dwisaty4@yahoo.com>
2014-05-18 09:38:29 +0700

    Udah bisa multirequest dari User, tapi masih belum realisasi pengiriman file dalam method tertentu

--- src/server/Server.java
+++ src/server/Server.java
@@ -11,6 +11,7 @@ import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.io.*;
 
 import Entity.Credential;
 import Entity.TransObject;
@@ -29,24 +30,6 @@ public class Server {
 		    ServerSocket serverSocket = new ServerSocket(portNumber);
                     while(true){
                         Socket clientSocket = serverSocket.accept();
-                        
-                        //buat kalo mau baca dari file
-                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-                        out.println(in.readLine());
-                        out.println("Message received");
-                        BufferedOutputStream bout = new BufferedOutputStream(new FileOutputStream(new File("receive.pdf")));
-                        int bytesread;
-                        byte[] outBuf = new byte[2048];
-                        int retval;
-                        do{
-                                retval = (bytesread = clientSocket.getInputStream().read(outBuf));
-                                System.out.println(retval);
-                                bout.write(outBuf, 0, bytesread);
-                                System.out.println(bytesread);
-                                System.out.println("dummy1");
-                        }while(retval == BUF_SIZE);
-                        
                         ServerThread ss = ServerManager.get(clientSocket);
                         ss.start();
                     }
@@ -56,4 +39,10 @@ public class Server {
 		}
 	}
 }
+
+/*
+
+//buat kalo mau baca dari file
+                        
+*/
 
