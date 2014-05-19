69d25bb856203146238cededbcc9a44011cc084d
Winson <13512071@std.stei.itb.ac.id>
2014-05-19 15:28:14 +0700

    tambah suara lagi

--- src/engine/GUIScenes/LevelMenuGUI.java
+++ src/engine/GUIScenes/LevelMenuGUI.java
@@ -267,6 +267,7 @@ public class LevelMenuGUI extends Scene
         else if (backRect.contains(event.getPoint()))
         {
             menuPressed = 2;
+            SoundManager.playSound(arrowSound);
             System.out.println("Back Button");
         }
     }
