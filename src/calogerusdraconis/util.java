2c8099e45ec5bbd4abe734c3b879c6498283c20a
willy2706 <willylie2706@gmail.com>
2014-05-19 23:02:25 +0700

    final

--- src/calogerusdraconis/util.java
+++ src/calogerusdraconis/util.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 dalva
+ * Copyright (C) 2014 Calogerus Draconis Team
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
